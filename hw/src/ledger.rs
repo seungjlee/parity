821e21b381cfa41ec1073345579b71a034ff1131
niklasad1 <niklasadolfsson1@gmail.com>
2018-03-14 14:48:26 +0100

    fix ledger test

--- hw/src/ledger.rs
+++ hw/src/ledger.rs
@@ -446,7 +446,9 @@ fn smoke() {
 	use rustc_hex::FromHex;
 	let hidapi = Arc::new(Mutex::new(hidapi::HidApi::new().unwrap()));
 	let manager = Manager::new(hidapi.clone(), Arc::new(AtomicBool::new(false))).unwrap();
-	manager.update_devices().unwrap();
+
+	assert_eq!(try_connect_polling(manager.clone(), Duration::from_millis(500)), true);
+
 	for d in &*manager.devices.read() {
 		println!("Device: {:?}", d);
 	}
